4fe3708731bc2d1e5f3f27769b2a8cf6625cbd5e
nikitapatil311 <patilnikitansp0@gmail.com>
2023-02-07 14:59:53 +0100

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const btne1 = document.querySelector(".btn");
+
+btne1.addEventListener("mouseover", (event) => {
+    const x = event.pageX - btne1.offsetLeft;
+    const y = event.pagey - btne1.offsetTop;
+
+    btne1.style.setProperty("--xPos" , x + "px");
+    btne1.style.setProperty("--yPos", y + "px");
+
+});
